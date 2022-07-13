be866588ef424c401a6bc85c1daa651217c2a195
code-bebop <woonj2@gmail.com>
2022-07-13 19:23:16 +0900

    라이브러리 최신화 후 오류 나는 부분 해결
    라이브러리 최신화를 한 다음 문법 오류가 발생한 것을 해결했다.
    - `react` 업데이트 후 JSX transport를 적용한 부분에 문법 오류가 발생한 것 해결

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes } from "react-router-dom";
 
 const Router = () => {
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import media from "../media";
 
